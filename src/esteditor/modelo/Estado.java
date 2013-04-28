d294cdaaf4a20a61376f514d4d2c6d3fe7385fb5
paulofer85 <paulofer85@gmail.com>
2013-04-28 19:41:39 -0300

    +estado

--- src/esteditor/modelo/Estado.java
+++ src/esteditor/modelo/Estado.java
@@ -4,12 +4,6 @@ import mereditor.modelo.Entidad;
 
 public class Estado extends Entidad {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
