01026c8c6bd807176d5c5cf7481d659c5ea456a4
paulofer85 <paulofer85@gmail.com>
2013-06-25 01:45:23 -0300

    +validaciones de proyecto vacio

--- src/mereditor/modelo/validacion/ValidarEquilibrioAtributos.java
+++ src/mereditor/modelo/validacion/ValidarEquilibrioAtributos.java
@@ -20,25 +20,35 @@ public class ValidarEquilibrioAtributos implements Validacion {
 		List<ComponenteAtributos> componentes = new ArrayList<>();
 		
 		//FIXME puede que en la cuenta de equilibrio no se tengan que incluir los estados
-		componentes.addAll(proyecto.getEstados());
-		componentes.addAll(proyecto.getEntidades());
-		componentes.addAll(proyecto.getRelaciones());
+		if(proyecto.getEstados() != null)
+			componentes.addAll(proyecto.getEstados());
+		
+		if(proyecto.getEntidades() != null)
+			componentes.addAll(proyecto.getEntidades());
+		
+		if(proyecto.getRelaciones() != null)
+			componentes.addAll(proyecto.getRelaciones());
 
-		int total = 0;
-
-		for (ComponenteAtributos componenteAtrs : componentes)
-			total += componenteAtrs.getAtributos().size();
-
-		int promedio = total / componentes.size();
-
-		for (ComponenteAtributos componenteAtrs : componentes) {
-			int delta = promedio - componenteAtrs.getAtributos().size();
-			if (Math.abs(delta) > Validacion.MAX_DESVIACION_ATRIBUTOS) {
-				String msj = "Tiene %d atributos mientras que el promedio es %d.";
-				msj = String.format(msj, componenteAtrs.getAtributos().size(), promedio);
-				observaciones.add(new Observacion((Componente) componenteAtrs, msj));
+		if(componentes.size() > 0)
+		{
+			int total = 0;
+	
+			for (ComponenteAtributos componenteAtrs : componentes)
+				total += componenteAtrs.getAtributos().size();
+	
+			int promedio = total / componentes.size();
+	
+			for (ComponenteAtributos componenteAtrs : componentes) {
+				int delta = promedio - componenteAtrs.getAtributos().size();
+				if (Math.abs(delta) > Validacion.MAX_DESVIACION_ATRIBUTOS) {
+					String msj = "Tiene %d atributos mientras que el promedio es %d.";
+					msj = String.format(msj, componenteAtrs.getAtributos().size(), promedio);
+					observaciones.add(new Observacion((Componente) componenteAtrs, msj));
+				}
 			}
 		}
+		else
+			observaciones.add(new Observacion("El proyecto se encuentra vacio"));
 
 		return observaciones;
 	}
